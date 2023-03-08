39d26635a8cd514edece84eae3e279752190fd38
tiagomota79 <tmota@cloudraker.com>
2023-03-08 10:24:49 -0500

    Basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import OtpInput from "./components/otpInput";
 
 function App() {
+  const [otp, setOtp] = useState("");
+  const onChange = (value: string) => setOtp(value);
+
   return (
     <div className="App">
-      <h1 className="">Test</h1>
+      <h1 className="">OTP Test</h1>
+      <form id="otp" className="w-80">
+        <OtpInput value={otp} valueLength={6} onChange={onChange} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
